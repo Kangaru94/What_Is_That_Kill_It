9c30dec398377a60652963a4b64e052c95e8cc95
Kangaru <46432989+Kangaru94@users.noreply.github.com>
2020-03-01 08:23:56 -0600

    Add files via upload

---
+++ LightToggle.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Messing around with creating my own classes
+// Serialiazable makes it available in unity window
+[System.Serializable]
+public class MyData
+{
+    public int ValueOne;
+    public int ValueTwo;
+}
+public class LightToggle : MonoBehaviour
+{
+    public Light myLight;
+    public MyData DataClass;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       if (myLight.enabled == false)
+        {
+            myLight.enabled = true;
+            Debug.Log("Let there be light!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.L))
+        {
+            LightSwitch();
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            SpaceTest();
+        }
+    }
+
+    void LightSwitch()
+    {
+        myLight.enabled = !myLight.enabled;
+
+       if (myLight.enabled == true)
+        {
+            Debug.Log("Light toggled on");
+        }
+       else
+        {
+            Debug.Log("Light toggled off");
+        }
+
+    }
+
+    void SpaceTest()
+    {
+        Debug.Log("You've pressed space, congrats, I guess.");
+    }
+}
---
+++ playControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playControl : MonoBehaviour
+{
+
+    public float PlayerSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm a cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            RightPress();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            LeftPress();
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            UpPress();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            DownPress();
+        }
+
+        if (Input.GetKey("space"))
+        {
+            Jump();
+        }
+    }
+
+    void RightPress()
+    {
+        transform.Translate(PlayerSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    void LeftPress()
+    {
+        transform.Translate(-PlayerSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    void UpPress()
+    {
+        transform.Translate(0f, 0f, PlayerSpeed * Time.deltaTime);
+    }
+
+    void DownPress()
+    {
+        transform.Translate(0f, 0f, -PlayerSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        transform.Translate(0f, 5f * Time.deltaTime, 0f);
+    }
+}
